692bf8b1968bff1b9a58b80fae60366dc86dbef6
ankitsaini000 <chintusaini9999@gmail.com>
2025-08-10 09:43:25 +0530

    Initial commit

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,60 @@
-export default {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:5001/api/:path*',
-      },
-    ];
+// export default {
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: 'http://localhost:5001/api/:path*',
+//       },
+//     ];
+//   },
+// }; 
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      'localhost',
+      'images.unsplash.com',
+      'i.pinimg.com',
+      'img.freepik.com',
+      'via.placeholder.com',
+      'res.cloudinary.com',
+      'platform-lookaside.fbsbx.com',
+      'graph.facebook.com',
+      'dgzcfva4b.cloudinary.net'
+    ],
   },
-}; 
+  compiler: {
+    styledComponents: true,
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+  async rewrites() {
+    if (process.env.NODE_ENV === 'development') {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost:5001/api/:path*',
+        },
+      ];
+    }
+    return [];
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.css$/i,
+      use: [
+        'style-loader',
+        'css-loader',
+        'postcss-loader'
+      ],
+    });
+    return config;
+  },
+};
+
+export default nextConfig;
